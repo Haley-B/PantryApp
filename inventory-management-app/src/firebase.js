ebba2d8126371737f61f5e7b8dc53bf0be306592
Haley-B <haleyboswell046@gmail.com>
2024-07-31 22:16:14 -0700

    sfsd

--- inventory-management-app/src/firebase.js
+++ inventory-management-app/src/firebase.js
@@ -1,11 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore"; // Add this line
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDw9ZVNmiQi2S9ZupyjjiidzbuE5IlEX-k",
   authDomain: "pantry-app-5f3d9.firebaseapp.com",
@@ -19,4 +17,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-export { firestore };
+const firestore = getFirestore(app); // Initialize Firestore
+
+export { firestore }; // Export Firestore
